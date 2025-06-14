7f9bcbf077e640f5b12814d433b00004c2035ad6
deresegetachew <deresegetachew@gmail.com>
2025-06-14 06:29:57 +0200

    fix merge issues

--- libs/common/src/dtos/index.ts
+++ libs/common/src/dtos/index.ts
@@ -1,5 +1,3 @@
 export * from './email-workers';
 export * from './core-service';
-export * from './emailThreads.response.dto';
-export * from './messageIntent.response.dto';
 export * from './document-worker/uploadDocument.dto';
