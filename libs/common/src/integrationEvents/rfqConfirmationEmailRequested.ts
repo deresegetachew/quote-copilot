ff5f5d3ba6de1b0e007499daabf0194e75a91bdb
Abenezer Bayu <abenezer89bayu@gmail.com>
2025-06-14 10:41:36 +0300

    integration event wip

--- libs/common/src/integrationEvents/rfqConfirmationEmailRequested.ts
+++ libs/common/src/integrationEvents/rfqConfirmationEmailRequested.ts
@@ -7,7 +7,19 @@ import {
 } from '../types/integrationEvents.type';
 
 export class RfqConfirmationEmailRequested implements IntegrationEvent {
+  timestamp: Date;
+  source: string;
+  version: string;
+  metadata: IntegrationEventMetadata;
+  payload: TRfqConfirmationEmailRequestedPayload;
+
   constructor(data: TRfqConfirmationEmailRequestedPayload, source: TEventSources) {
-    super(rfqConfirmationEmailRequestedSubject, data, source);
+    this.timestamp = new Date();
+    this.source = source;
+    this.version = '1.0.0';
+    this.metadata = {
+      eventId: `rfq-confirmation-${Date.now()}`,
+    };
+    this.payload = data;
   }
 } 
